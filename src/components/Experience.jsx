44cfc89d00e6b89173439bea798237936af3d7a3
Jacques Adams <adamsjacques546@gmail.com>
2023-07-04 11:04:00 -0400

    ch 7/4/2023

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,15 +6,15 @@ const Experience = () => {
       <div className='content-container'>
         <h1>Experience</h1>
         <h2>Contract Technical Consultant for Metamechanics</h2>
-        <h3>2023 - Present</h3>
+        <h3>June 2023 - Present</h3>
         <ul>
           <li>
-            <p>Helped remove malware from Company Website, and made edits to site using WordPress.</p>
+            <p>Analyze and identify security risks for company website. Present and recommend solutions. Improve the company website's user experience.</p>
           </li>
         </ul>
 
         <h2>Student Clerk, Visual Arts Building, NJCU</h2>
-        <h3>2019 - 2023</h3>
+        <h3>September 2020 - May 2023</h3>
         <ul>
           <li>
             <p>Developed a database application for student locker assignments. Assisted with office work such as filing, creating schedules, and populating spreadsheets.</p>
@@ -23,7 +23,7 @@ const Experience = () => {
         
 
         <h2>Intern at the Office of innovation, City Hall</h2>
-        <h3>Summer of 2019</h3>
+        <h3>July 2019 - August 2019</h3>
 
         <ul>
           <li>
@@ -33,7 +33,7 @@ const Experience = () => {
         
 
         <h2>Computer Science Tutor</h2>
-        <h3>2018 - Present</h3>
+        <h3> June 2018 - Present</h3>
 
         <h1>Volunteer Work</h1>
         <h2>Audio Video Technician, For religious organization</h2>
@@ -41,7 +41,7 @@ const Experience = () => {
 
         <ul>
           <li>
-          <p>Provided support to the AV team and helped broadcast meetings through video conferencing software like Zoom.</p>
+          <p>Provided support to the AV team and helped broadcast meetings through video conferencing software like Zoom. Assist and train new volunteers</p>
           </li>
         </ul>
         
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,11 +5,11 @@ const Profile = () => {
     <div>
       <div className='content-container'>
         <h1>Summary</h1>
-        <p>I am a computer science enthusiast, and not just to get a job. I enjoy it. In my free time, I work on programming projects and experiment with computers and other electronics. I enjoy problem solving and taking on projects that are bigger than me.</p>
+        <p>I am a computer science enthusiast, and not just to get a job. I enjoy it. In my free time I work on programming projects and experiment with computers and other electronics. I enjoy problem solving and taking on projects that are bigger than me.</p>
         
         <h1>Education</h1>
         <h2>Bachelor of Computer Science, New Jersey City University</h2>
-        <h3>2019 - 2023 | GPA: 3.5</h3>
+        <h3>September 2019 - May 2023 | GPA: 3.5</h3>
       </div>
 
     </div>
