03bdf5a7725d11b495724ae8e008e72df63ed36f
Jacques Adams <adamsjacques546@gmail.com>
2023-09-23 18:48:51 -0400

    add new contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import addressicon from '../images/address.png'
 import emailicon from '../images/email.png'
 import phoneicon from '../images/phone.png'
+import linkedinicon from '../images/linkedin.png'
+import githubicon from '../images/github.png'
 const Contact = () => {
   return (
     <div>
@@ -20,6 +22,16 @@ const Contact = () => {
         <div>
           <img style={{display:'inline-block'}}src={addressicon}></img>
           <h2 style={{display:'inline-block'}}>Jersey City New Jersey</h2>
+        </div>
+
+        <div>
+          <img style={{display:'inline-block'}}src={linkedinicon}></img>
+          <a href='https://www.linkedin.com/in/jacques-adams-20036b213/'><h2 style={{display:'inline-block'}}>jacques-adams-20036b213</h2></a>
+        </div>
+
+        <div>
+          <img style={{display:'inline-block'}}src={githubicon}></img>
+          <h2 style={{display:'inline-block'}}>https://github.com/ASwitchCase</h2>
         </div>
         
       </div>
